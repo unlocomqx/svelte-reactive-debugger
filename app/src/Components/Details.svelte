3a9ba1b7ab57ffc52801f6c87430f692f30310b9
unlocomqx <amxmodx.unloco@gmail.com>
2021-01-03 14:22:39 +0100

    Extract Resize.svelte

--- app/src/Components/Details.svelte
+++ app/src/Components/Details.svelte
@@ -1,32 +1,10 @@
 <script lang="ts">
   import { ui_store } from "../store";
+  import Resize from "./Resize.svelte";
 
   let details_div: HTMLDivElement;
   let details_width: number;
-
-  let resizing = false;
-  let start_point = null;
-  let start_width = null;
-  function startResize (ev: MouseEvent) {
-    resizing = true;
-    start_point = ev.clientX;
-    start_width = details_div.clientWidth;
-    document.body.classList.add("noselect");
-  }
-
-  function resize (ev) {
-    if (resizing) {
-      details_width = Math.max(start_width + (start_point - ev.clientX), 100);
-    }
-  }
-
-  function stopResize () {
-    resizing = false;
-    document.body.classList.remove("noselect");
-  }
 </script>
-
-<svelte:window on:mousemove={resize} on:mouseup={stopResize}/>
 
 <div id="details" bind:this={details_div} style="width: {details_width}px;">
   <div id="details-header">
@@ -35,7 +13,7 @@
   <div id="details-body">
     {$ui_store.inspected_item}
   </div>
-  <div id="details_resize" on:mousedown={startResize}></div>
+  <Resize {details_div} bind:details_width/>
 </div>
 
 <style>
@@ -59,14 +37,5 @@
 
   #details-body {
     padding: 5px;
-  }
-
-  #details_resize {
-    position: absolute;
-    top: 0;
-    left: 0;
-    height: 100%;
-    width: 2px;
-    cursor: ew-resize;
   }
 </style>
---
+++ app/src/Components/Resize.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+
+  export let details_div: HTMLDivElement;
+  export let details_width: number;
+
+  let resizing = false;
+  let start_point = null;
+  let start_width = null;
+  function startResize (ev: MouseEvent) {
+    resizing = true;
+    start_point = ev.clientX;
+    start_width = details_div.clientWidth;
+    document.body.classList.add("noselect");
+  }
+
+  function resize (ev) {
+    if (resizing) {
+      details_width = Math.max(start_width + (start_point - ev.clientX), 100);
+    }
+  }
+
+  function stopResize () {
+    resizing = false;
+    document.body.classList.remove("noselect");
+  }
+</script>
+
+<svelte:window on:mousemove={resize} on:mouseup={stopResize}/>
+
+<div id="details_resize" on:mousedown={startResize}></div>
+
+<style>
+  #details_resize {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 2px;
+    cursor: ew-resize;
+  }
+</style>
