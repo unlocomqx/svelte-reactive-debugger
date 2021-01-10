2985ea918ce5b429f5bfcbdce270d5055a84201d
unlocomqx <amxmodx.unloco@gmail.com>
2021-01-10 17:56:37 +0100

    Remove livereload

--- app/rollup.config.js
+++ app/rollup.config.js
@@ -1,11 +1,10 @@
 import svelte from "rollup-plugin-svelte";
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
-import livereload from "rollup-plugin-livereload";
 import { terser } from "rollup-plugin-terser";
 import css from "rollup-plugin-css-only";
 import sveltePreprocess from "svelte-preprocess";
-import typescript from '@rollup/plugin-typescript';
+import typescript from "@rollup/plugin-typescript";
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -73,10 +72,6 @@ export default {
     // In dev mode, call `npm run start` once
     // the bundle has been generated
     !production && serve(),
-
-    // Watch the `public` directory and refresh the
-    // browser on changes when not in production
-    !production && livereload("../dist"),
 
     // If we're building for production (npm run build
     // instead of npm run dev), minify
